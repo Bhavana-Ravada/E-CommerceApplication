93262787307762de0dbf46308f5ff9ef78cfc90f
bhavana ravada <ravadabhavana3@gmail.com>
2026-02-24 15:15:10 +0530

    updated

--- demo/src/main/java/com/ecom/demo/Contoller/ProductController.java
+++ demo/src/main/java/com/ecom/demo/Contoller/ProductController.java
@@ -3,10 +3,7 @@ package com.ecom.demo.Contoller;
 import com.ecom.demo.Model.Product;
 import com.ecom.demo.Service.ProductsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
@@ -21,5 +18,10 @@ public class ProductController {
     @GetMapping("/products/{prodId}")
     Product getProductsById(@PathVariable int prodId){
         return prodServ.getProductsById(prodId);
+    }
+
+    @PostMapping("/Products")
+    void addProducts(@RequestBody Product prod){
+        prodServ.addProduct(prod);
     }
 }
--- demo/src/main/java/com/ecom/demo/Service/ProductsService.java
+++ demo/src/main/java/com/ecom/demo/Service/ProductsService.java
@@ -3,21 +3,26 @@ package com.ecom.demo.Service;
 import com.ecom.demo.Model.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 @Service
 public class ProductsService {
-    List<Product> dummyProducts= Arrays.asList(
+    List<Product> dummyProducts= new ArrayList<>(Arrays.asList(
             new Product(1,"mobile",50000),
             new Product(2,"laptop",90000),
             new Product(1,"cosmetics",10000)
-            );
-   public List<Product> getProducts(){
+            ));
+   public List<Product> getProducts() {
         return dummyProducts;
     }
     public Product getProductsById(int prodId){
        return dummyProducts.stream()
                .filter(p ->p.getProdId()==prodId)
                .findFirst().get();
+    }
+
+    public void addProduct(Product prod) {
+       dummyProducts.add(prod);
     }
 }
